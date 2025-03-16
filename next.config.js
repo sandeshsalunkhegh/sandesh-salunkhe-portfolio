1ad8f23709ae3545178cb1e14855224de1cdacd3
Sandesh Salunkhe <sandy.decoder@gmail.com>
2025-03-17 04:06:26 +0530

    Updates next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    output: 'standalone', // Ensures Netlify builds the correct output
+    distDir: '.next',
+    output: 'export',
   }
   
   module.exports = nextConfig
